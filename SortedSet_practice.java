c7625ee682b3886b004243b83c74292e238e2ecc
Anurag Kumar <anuragkumar07072002@gmail.com>
2025-01-02 21:13:06 +0530

    practised sortedSet

--- SortedSet_practice.java
+++ SortedSet_practice.java
@@ -1,2 +1,24 @@
-public class TreeSet_practice {
+import java.util.*;
+public class SortedSet_practice {
+    public static void main(String[] args) {
+        SortedSet<Integer>s=new TreeSet<>();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        s.add(4);
+        s.add(50);
+        s.add(10);
+        System.out.println(s);
+        System.out.println(s.first());
+        System.out.println(s.last());
+        System.out.println("subset "+s.subSet(2,50));
+        System.out.println("tailSet "+s.tailSet(3));
+        System.out.println("headSet "+s.headSet(50));
+    }
 }
+//[1, 2, 3, 4, 10, 50]
+//        1
+//        50
+//subset [2, 3, 4, 10]
+//tailSet [3, 4, 10, 50]
+//headSet [1, 2, 3, 4, 10]
