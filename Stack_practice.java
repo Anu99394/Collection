50cfe38a9c402da0023b4119a31cdd2f181983fd
Anurag Kumar <anuragkumar07072002@gmail.com>
2025-01-02 17:32:20 +0530

    practised Stackk

--- Stack_practice.java
+++ Stack_practice.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 public class Stack_practice {
+    public static void main(String[] args) {
+        Stack<Integer> s=new Stack<>();
+        s.add(2);
+        s.add(3);
+        s.add(5);
+        s.add(20);
+        System.out.println(s);
+        s.remove(2);
+        s.pop();
+        System.out.println(s);
+    }
 }
+
+//[2, 3, 5, 20]
+//        [2, 3]
--- Vector_practice.java
+++ Vector_practice.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 public class Vector_practice {
+    public static void main(String[] args) {
+        Vector<Integer>v=new Vector<>();
+        v.add(2);
+        v.add(3);
+        v.add(4);
+        v.add(5);
+        v.remove(2);
+        System.out.println(v);
+
+    }
 }
+//[2, 3, 5]
